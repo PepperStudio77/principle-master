6e64c32b0fa4a79cca07b6a51935ca41ed53e185
gaoming <gaoming.cs@gmail.com>
2025-05-13 15:30:51 +0800

    chore: remove the redundant code

--- principle-master/main.py
+++ principle-master/main.py
@@ -5,8 +5,6 @@ from llama_index.core import Settings
 
 from core.index import create_and_persist_index_from_path
 from core.workflow import run_customise_workflow
-from multiagent.workflow import run
-from singleagent.full_context import chat
 from utils.llm import get_embedding
 
 
@@ -14,19 +12,6 @@ from utils.llm import get_embedding
 def consult():
     click.echo("Principle Master is ready!")
     pass
-
-
-@click.command()
-def single_agent():
-    response = chat()
-    click.echo(response.text)
-
-
-@click.command()
-@click.argument("enquiry")
-@click.option('--verbose', is_flag=True, help='Enable verbose mode')
-def multi_agent(enquiry, verbose):
-    asyncio.run(run(enquiry, verbose=verbose))
 
 
 @click.command()
@@ -47,8 +32,6 @@ def index_content(pdf_path, verbose):
     print(f" Index completed. ")
 
 
-consult.add_command(single_agent)
-consult.add_command(multi_agent)
 consult.add_command(principle_master)
 consult.add_command(index_content)
 # consult.add_command(chat)
